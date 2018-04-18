0db7fae9dc765d1827d196d4ccee0cd928b4a7f6
Sarah Spikes <sarah@effectivealtruism.org>
2018-04-18 01:13:20 -0700

    rake db:migrate after omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_18_080217) do
+ActiveRecord::Schema.define(version: 2018_04_18_081234) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 2018_04_18_080217) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
