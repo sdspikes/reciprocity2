a3ebfc19582b0f094b79197a7e741a1b6a5111c3
Sarah Spikes <sarah@effectivealtruism.org>
2018-04-18 00:57:45 -0700

    route root to categories index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'categories#index'
 end
