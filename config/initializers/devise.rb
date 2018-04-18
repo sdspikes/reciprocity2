d8bd603002a9f81829402ac9bcd6090c1b37687e
Sarah Spikes <sarah@effectivealtruism.org>
2018-04-18 14:25:33 -0700

    whatever i just did

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,7 +258,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :facebook, ENV['TEST_FB_APP_ID'], ENV['TEST_FB_APP_SECRET'], callback_url: "https://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, ENV['TEST_FB_APP_ID'], ENV['TEST_FB_APP_SECRET'], callback_url: "https://www.localhost:3000/users/auth/facebook/callback"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
