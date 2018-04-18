3be93ebe5490434b70f0fa40eb0d892b01738e9a
Sarah Spikes <sarah@effectivealtruism.org>
2018-04-18 14:34:36 -0700

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_04_18_081234) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.text "description"
