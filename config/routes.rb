041ca72ad58d1afa149d81dcd3db1e9975db4ce8
Sarah Spikes <sarah@effectivealtruism.org>
2018-06-12 23:38:02 -0700

    make the checks page the root and have that be where you go when you click on your username

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'categories#index'
+  root 'checks#index'
 end
