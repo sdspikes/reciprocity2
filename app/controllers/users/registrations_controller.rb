ddf7765552faae6a1c37c97585a13fe5d188120e
Sarah Spikes <sarah@effectivealtruism.org>
2018-11-25 22:30:25 -0800

    allow users to add names when creating or editing accounts manually

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create
     @user = User.new(user_params)
-
   end
 
   private
@@ -13,6 +12,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     def user_params
       params.require(:user).permit!
-      # devise_parameter_sanitizer.for(:sign_up).push(:name, :phone, :organization)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'compatibilities#index'
+  # root 'compatibilities#index'
+  root 'checks#index'
 end
