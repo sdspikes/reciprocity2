2aff7ae403750ba1a76380ea0cfa43c72ad7a8f4
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2019-02-16 12:45:16 -0800

    Add spec for creating a privacy group

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,7 +16,6 @@ end
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
-  config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
 end
