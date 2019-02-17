c768e7192acc1d51706dc27101e3ec14d1c51bc6
Sarah Spikes <sarah@effectivealtruism.org>
2019-02-16 17:05:58 -0800

    only show items that the viewer is authorized to see

--- app/models/profile_item.rb
+++ app/models/profile_item.rb
@@ -3,5 +3,17 @@ class ProfileItem < ApplicationRecord
   belongs_to :privacy_group, optional: true
   belongs_to :profile_item_category
   belongs_to :profile_item_data, polymorphic: true
-  accepts_nested_attributes_for :profile_item_data, update_only: true
+  accepts_nested_attributes_for :profile_item_data
+
+  def self.get_viewable(user, viewer)
+    # only show if there's no user group set (public) or the viewer is in a privacy groups belonging to the user
+    user.profile_items.to_a.select do |item|
+      !item.privacy_group || item.privacy_group.privacy_group_members.pluck(:user_id).include?(viewer.id)
+    end
+  end
+
+  def build_profile_item_data(params)
+    raise "Unknown profile_item_data_type: #{profile_item_data_type}" unless PROFILE_ITEM_DATA_TYPES.include?(profile_item_data_type)
+    self.profile_item_data = profile_item_data_type.constantize.create(params)
+  end
 end
