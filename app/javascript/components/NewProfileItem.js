0a3dd69a20c1b609a4a19c1a88fa0b8791fdbaf4
Sarah Spikes <sarah@effectivealtruism.org>
2019-06-23 14:24:42 -0700

    add button to save new profile item instead of doing it automatically on change

--- app/javascript/components/NewProfileItem.js
+++ app/javascript/components/NewProfileItem.js
@@ -13,21 +13,27 @@ class NewProfileItem extends React.Component {
     let categories = props.categories;
     this.state = {
       categories: categories,
-      selected_category_id: Object.keys(categories).filter(id => !categories[id].used)[0],
       editing: false,
       new_item: null,
       options: [],
     }
+    this.state.selected_category_id = this.getUnusedCategories()[0]
+
   }
 
   render () {
     const {editing, categories, selected_category_id} = this.state
+    const unused_categories = this.getUnusedCategories()
     if (!editing) {
+      if (unused_categories.length === 0) {
+        // TODO(sdspikes): remove for beta
+        return <div>No more categories.  If you would like to suggest a new category, do so on <a href="https://docs.google.com/document/d/13miL_j8WlWRfbQ0FNo3SKhHRYQm8dTUBJYjU7xV5IR4/edit#heading=h.17eq8q7kyrk4" target='_blank'>this doc</a>.</div>
+      }
       return <span id="new_item">
         <select
           value={selected_category_id}
           onChange={(e) => this.updateCategoryId(e.target.value)}>
-          {Object.keys(categories).filter(id=>!categories[id].used).map((id) =>
+          {unused_categories.map((id) =>
               <option key={id} id={id} value={id}>{categories[id].title}</option>
           )}
         </select>
@@ -43,15 +49,19 @@ class NewProfileItem extends React.Component {
 
     return <div className="box">
       <span>{title}:&nbsp;</span>
-        {item_type == "multi" && <select
+        {item_type == "multi" && <span>
+          <select
             value={new_item.profile_item_data_id}
-            onChange={(e) => this.updateValue(e.target, item_type)}>
+            onChange={(e) => this.updateOptionId(e.target, item_type)}>
             {options.map((option) =>
                 <option key={option.id} id={option.id} value={option.id}>{option.value}</option>
               )
             }
           </select>
-        }
+          <button onClick={(e) => this.updateValue()}>
+            Set Value
+          </button>
+        </span>}
         {item_type == "text" && <span>
           <input
             value={new_item.value || ""}
@@ -64,11 +74,28 @@ class NewProfileItem extends React.Component {
     </div>;
   }
 
+  getUnusedCategories () {
+    const {categories} = this.state
+    return Object.keys(categories).filter(id => !categories[id].used)
+  }
+
+  componentDidUpdate () {
+    const {categories, selected_category_id} = this.state
+    if (!selected_category_id) {
+      const unused_categories = this.getUnusedCategories()
+      if (unused_categories.length) {
+        this.setState({
+          selected_category_id: unused_categories[0],
+        })
+      }
+    }
+  }
+
   resetState () {
     const {categories, selected_category_id} = this.state
     categories[selected_category_id].used = true
     this.setState({
-      selected_category_id: Object.keys(categories).filter(id => !categories[id].used)[0],
+      selected_category_id: this.getUnusedCategories()[0],
       editing: false,
       new_item: null,
       options: [],
@@ -79,7 +106,6 @@ class NewProfileItem extends React.Component {
     const {new_item, categories, selected_category_id} = this.state
     createTextItem(new_item.value, (json_response) => {
       new_item.profile_item_data_id = json_response.id
-      new_item.profile_item_data_type = category_type_to_item_type[item_type]
 
       this.resetState()
 
@@ -88,19 +114,24 @@ class NewProfileItem extends React.Component {
     })
   }
 
-  updateValue (target, item_type) {
+  updateValue () {
+    const {new_item, options} = this.state
+
+    this.resetState()
+
+    newProfileItem(new_item, (newItem) =>
+        this.props.callback(newItem, options.find(o => o.id === new_item.profile_item_data_id), options))
+  }
+
+  updateOptionId (target, item_type) {
     var index = target.selectedIndex;
     var optionElement = target.childNodes[index]
     var option_id =  Number.parseInt(optionElement.getAttribute('id'));
 
     const {new_item, options, categories, selected_category_id} = this.state
     new_item.profile_item_data_id = option_id
-    new_item.profile_item_data_type = category_type_to_item_type[item_type]
 
-    this.resetState()
-
-    newProfileItem(new_item, (newItem) =>
-        this.props.callback(newItem, options.find(o => o.id === option_id), options))
+    this.setState({new_item: new_item})
   }
 
   updateText (newValue) {
@@ -116,11 +147,15 @@ class NewProfileItem extends React.Component {
   createNewItem () {
     const {selected_category_id, categories} = this.state
 
+    const item_type = categories[selected_category_id].item_type
     const options = (categories[selected_category_id] && categories[selected_category_id].options) || []
 
+    const data_id = 0 || (options[0] && options[0].id)
     this.setState({
       new_item: {
-        profile_item_category_id: selected_category_id
+        profile_item_category_id: selected_category_id,
+        profile_item_data_id: data_id,
+        profile_item_data_type: category_type_to_item_type[item_type]
       },
       editing: true,
       options
